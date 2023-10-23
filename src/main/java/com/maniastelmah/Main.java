4f4971790633cfa46f9d8d08f0013c220f17513c
Maria Stelmakh <maniastelmah@gmail.com>
2023-10-23 22:14:34 +0400

    Main method refactoring

--- src/main/java/com/maniastelmah/Main.java
+++ src/main/java/com/maniastelmah/Main.java
@@ -3,7 +3,8 @@ package com.maniastelmah;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String msg = "Hello world!";
+        System.out.println(msg);
 
     }
 }
